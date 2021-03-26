24deebd4c102593574354cbc3e9cb69472a49626
Gustavo Santos <47330736+Santos-Gustavo@users.noreply.github.com>
2021-03-26 15:48:01 +0100

    Update <5 kyu> Simple Pig Latin.py

--- <5 kyu> Simple Pig Latin.py
+++ <5 kyu> Simple Pig Latin.py
@@ -1,3 +1,7 @@
+''''
+Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+''''
+
 def pig_it(text):
     text = text.split()
     ftext=''
